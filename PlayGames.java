25d37f488571cd793b44ef6356aecbe113e70adc
Hunter <98huntermoore@gmail.com>
2020-11-16 16:48:25 -0500

    Udated menu so it's the same as original repo

--- PlayGames.java
+++ PlayGames.java
@@ -3,25 +3,39 @@ import java.util.Scanner;  // Import the Scanner class
 public class PlayGames {
     private static boolean playAgain = true;
     public static void main(String args[]){
+        boolean validInput = false;
         System.out.println("Welcome to the Game of Games! Please enter the number of the corresponding game you wish to play:");
         System.out.println("\n");
 
         while(playAgain == true){
-            System.out.println("\n 1 - Find the red thread;\n 2 - Guess the number;\n 3 - Find the thimble;\n 4 - Even or Odd;\n 5 - Coin flip");
-            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-            int gameChosen = scanner.nextInt();  // Read user input
-            if(gameChosen == 1){
-                FindTheRedThread.playGame(); 
-            }
-            if(gameChosen == 2){
-                GuessTheNumber.playGame(); 
-            }
-            if(gameChosen == 3){
-                Thimble.gameThimble();
-            }
-            if(gameChosen == 4){
-                EvenOdd.rules();
-                EvenOdd.PlayGame();
+            System.out.println("\n 1 - Find the red thread;\n 2 - Guess the number;\n 3 - Find the thimble;\n 4 - Even or Odd;\n 5 - Coin flip\n");
+           while(!validInput){
+                int gameChosen = GetInput.numericInput();  // Read user input
+            
+                if(gameChosen == 1){
+                    FindTheRedThread.playGame(); 
+                    validInput = true;
+                }
+                else if(gameChosen == 2){
+                    GuessTheNumber.playGame(); 
+                    validInput = true;
+                }
+                else if(gameChosen == 3){
+                    Thimble.gameThimble();
+                    validInput = true;
+                }
+                else if(gameChosen == 4){
+                    EvenOdd.rules();
+                    EvenOdd.PlayGame();
+                    validInput = true;
+                }
+                else if(gameChosen == 5){
+                    CoinFlip.playGame();
+                    validInput = true;
+                }
+                else{
+                    System.out.println("Only input 1-5 is valid. Pick again.");
+                }
             }
 
             Scoreboard.printScoreboard();
