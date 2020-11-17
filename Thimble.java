696eb9b62a7fdeaa89e3de535c54da7eece00656
kpklotz <74474480+kpklotz@users.noreply.github.com>
2020-11-16 19:31:53 -0500

    Final Bug Code

--- Thimble.java
+++ Thimble.java
@@ -7,16 +7,19 @@
  */
 import java.util.Random;
 import java.util.Scanner;
-public class Thimble
+import java.util.InputMismatchException;
+public class Thimble 
 {
     // instance variables - replace the example below with your own
     static Scanner userInput = new Scanner(System.in);
+    static Scanner secUserInput = new Scanner(System.in);
+    static Scoreboard score = new Scoreboard();
     private static int bestOutNum;
     private static int hiderHand;
     private static int guessHand;
     private static int guesserScore;
     private static int hiderScore; 
-    
+
     /**
      * Constructor for objects of class Thimble
      */
@@ -25,49 +28,64 @@ public class Thimble
         // initialise instance variables
         guesserScore = 0;
         hiderScore = 0;
+        bestOutNum = 0;
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * Print out the rules of the find the Thimble game
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void rules()
     {
-        System.out.println("This game will be played in a 'best out of' fashion.");
-        System.out.println("First enter an odd number to represent the maximum number of games to be played.");
-        System.out.println("To play a single game, guess which hand your opponent is holding the thimble.");
-        System.out.println("If you guess correctly you win the game, if you guess incorrectly you lose.");
+        System.out.println("\t|-------------------------------------------------------------------------------------------|");
+        System.out.println("\t|                    Welcome to Find the Thimble                                            |");
+        System.out.println("\t|                                                                                           |");
+        System.out.println("\t|           Please read the description of the game here:                                   |");
+        System.out.println("\t|           1-Even odd game have to be played between two users (User and computer)         |");
+        System.out.println("\t|                                                                                           |");
+        System.out.println("\t|         2-This game will be played in a 'best out of' fashion.                            |");
+        System.out.println("\t|3- To play a single game, guess which hand your opponent is holding the thimble.           |");
+        System.out.println("\t|                                                                                           |");
+        System.out.println("\t|                                                                                           |");
+        System.out.println("\t|4-  If you guess correctly you win the game, if you guess incorrectly you lose.            |");
+        System.out.println("\t|-------------------------------------------------------------------------------------------|");
+        System.out.println();
+        System.out.println();
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * Printing an error message that can be used for all incorrect user inputs
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void guesserError(){
         System.out.println("Error: Invalid submission. Please reread directions.");
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * Getting the user to input the “best out of number” to know how many rounds the game will go for
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void setBestOutNum(){
-        System.out.println("Enter an odd number that is positive to represent the 'best out of number'.");
-        bestOutNum = userInput.nextInt();
-        if(bestOutNum % 2 == 0){
-            guesserError();
-            setBestOutNum();
-        }
-        System.out.println("Best out of number: " + bestOutNum);
+        while(bestOutNum % 2 != 1){
+            System.out.println("Enter an odd number that is positive to represent the 'best out of number'.");
+            try{
+                bestOutNum = userInput.nextInt();
+                if(bestOutNum % 2 == 0){
+                    throw new InputMismatchException();
+                }
+                System.out.println("Best out of number: " + bestOutNum);
+            }
+            catch(InputMismatchException e){
+                guesserError();
+                userInput.next();
+                System.out.println("Best out of number error: " + bestOutNum);
+            }
+        } 
     }
-     /**
-     * An example of a method - replace this comment with your own
+
+    /**
+     * This method will decide which hand the computer is hiding the thimble in and set the hiderHand variable
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void setHiderHand(){
         Random rand = new Random();
@@ -76,40 +94,50 @@ public class Thimble
         hiderHand = hiderHand + 1; //updating the value of hiderHand so that it is either 1 or 2
         System.out.println("cpu hand: " + hiderHand);
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * This method will accept the user input and set the guessHand variable
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void setGuess(){
-        System.out.println("Please enter 1 to guess the left hand or 2 to guess the right hand.");
-        guessHand = userInput.nextInt();
-        if(guessHand != 1 && guessHand != 2){
-            guesserError();
-            setGuess();
+        while(guessHand == 0){
+            System.out.println("Please enter 1 to guess the left hand or 2 to guess the right hand.");
+            try{
+                guessHand = secUserInput.nextInt();
+                if(guessHand != 1 && guessHand != 2){
+                    guesserError();
+                    setGuess();
+                }
+                System.out.println("Your guess: " + guessHand);
+            }
+            catch(InputMismatchException e){
+                guesserError();
+                secUserInput.next();
+            }
         }
-        System.out.println("Your guess: " + guessHand);
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * this method is meant to find out if the user correctly guessed the thimble.
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  int guessHand, this variable is representative of the hand the user is guessing
+     * @param  int hiderHand, this variable is representative of the hand the cpu is hiding the thimble in
+     * @return    Boolean, if the variables are equal this method will return true, otherwise it will return false
      */
     public static boolean compareHand(int guessHand,int hiderHand){
         if(guessHand == hiderHand){
             return true;
+            
         }
         else{
             return false;
         }
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * this method will update the score of either guesserScore or hiderScore depending on if the boolean is true or false. 
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  Boolean n, this will be the boolean from compareHand()
      */
     public static void updateScore(boolean n){
         if( n ==true){
@@ -119,39 +147,41 @@ public class Thimble
             hiderScore++;
         }
     }
+
     /**
-     * An example of a method - replace this comment with your own
+     * To identify if find the thimble is over based off the user’s score
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  guesserScore  this will be the score that the user has
+     * @return    Boolean, this will return true if the guesserScore has reached the parameters of the bestOutNum
+
      */
     public static boolean winnerGuesser(int guesserScore){
-        if(guesserScore == ((bestOutNum+1)/2)){
+        if(guesserScore == ((bestOutNum-1)/2)){
             return true;
         }
         else{
             return false;
         }
     }
-     /**
-     * An example of a method - replace this comment with your own
+
+    /**
+     * To identify if find the thimble is over based off the cpu’s score
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * @param  hiderScore  this will be the score that the cpu has
+     * @return Boolean, this will return true if the hiderScore has reached the parameters of the bestOutNum
      */
     public static boolean winnerHider(int hiderScore){
-         if(hiderScore == ((bestOutNum+1)/2)){
+        if(hiderScore == ((bestOutNum+1)/2)){
             return true;
         }
         else{
             return false;
         }
     }
-     /**
-     * An example of a method - replace this comment with your own
+
+    /**
+     * This method is a compolation of all the methods properly arranged so that the game can be quickly implemented
      *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
      */
     public static void gameThimble(){
         rules();
@@ -162,7 +192,19 @@ public class Thimble
             updateScore(compareHand(guessHand, hiderHand));
             winnerGuesser(guesserScore);
             winnerHider(hiderScore);
+            System.out.println("\t|-------------------------------------------------------------------------------------------|");
+            System.out.println("\t|           FIND THE THIMBLE: user score: " + guesserScore + "  cpu score: " + hiderScore +"                  |");
+            System.out.println("\t|                                                                                           |");
+            System.out.println("\t|-------------------------------------------------------------------------------------------|");
         }
-        System.out.println("user score: " + guesserScore + "  cpu score: " + hiderScore);
+        if(winnerGuesser(guesserScore) == true){
+            System.out.println("You won!");
+            score.userTotalScore++;
+        }
+        else{
+            System.out.println("The Computer won");
+            score.computerTotalScore++;
+        }
+        score.printScoreboard();
     }
 }
